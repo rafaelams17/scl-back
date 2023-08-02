033eaeddf9a32319e76a7091ea1a66cff5a0ed0a
Rafaela Santos <rafaelasantosminios@gmail.com>
2023-08-02 15:44:20 -0300

    implement local strategy validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
+import { UnauthorizedError } from './error/unauthorized.error';
 
 @Injectable()
 export class AuthService {
-  validateUser(email: string, password: string) {
-    throw new Error('Method not implemented.');
-  }
+  bcrypt = require('bcrypt');
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userService: UserService,
+  ) {}
+
   login() {
     return 'teste';
+  }
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findByEmail(email);
+
+    if (user) {
+      // verificar a senha informada com a hash que está no banco
+      const isPasswordValid = await this.bcrypt.compare(
+        password,
+        user.password,
+      );
+
+      if (isPasswordValid) {
+        return {
+          ...user,
+          password: undefined,
+        };
+      }
+    }
+    throw new UnauthorizedError(
+      'Email address or password provided is incorrect.',
+    );
   }
 }
